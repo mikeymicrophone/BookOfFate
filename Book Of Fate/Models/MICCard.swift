45da9345724f7184e7a249eeed7afa1c3db046e9
Mike Schwab <mschwab@cyrusinnovation.com>
2016-05-17 22:47:52 -0400

    birth cards can be calculated

---
+++ Book Of Fate/Models/MICCard.swift
@@ -0,0 +1,24 @@
+//
+//  MICCard.swift
+//  Book Of Fate
+//
+//  Created by Mike Schwab on 5/5/16.
+//  Copyright © 2016 MICharisma. All rights reserved.
+//
+
+import Foundation
+
+class MICCard {
+    var suit : String
+    var face : String
+    
+    init(suit : String, face : String) {
+        self.suit = suit
+        self.face = face
+    }
+    
+    class func birthCardForMonth(month : Int, day : Int) -> MICCard {
+        let position = 54 - ((month * 2) + day)
+        return MICSpread.default_card_stack().objectAtIndex(position) as! MICCard
+    }
+}
